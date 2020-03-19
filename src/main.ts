f54cc84d6fbd560a2515891fe455064e6c8a392c
Edward Thomson <ethomson@edwardthomson.com>
2020-03-19 14:38:24 +0000

    Initial revision

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+    try {
+        const token = process.env.GITHUB_TOKEN || ''
+        const projectName = core.getInput('name', { required: true })
+        const projectDescription = core.getInput('description')
+        const columns = core.getInput('columns')
+
+        const octokit = new github.GitHub(token)
+        let response
+
+        if (core.getInput('org')) {
+            response = await octokit.projects.createForOrg({
+                org: core.getInput('org'),
+                name: projectName,
+                body: projectDescription
+            })
+        } else {
+            let repo: string | undefined = core.getInput('repo')
+
+            if (!repo && !(repo = process.env.GITHUB_REPOSITORY))
+                throw new Error('could not determine correct repository')
+
+            if (!repo.includes('/'))
+                throw new Error('repo must be specified as owner/repo')
+
+            const components = repo.split('/', 2)
+
+            response = await octokit.projects.createForRepo({
+                owner: components[0],
+                repo: components[1],
+                name: projectName,
+                body: projectDescription
+            })
+        }
+
+        if (columns) {
+            for (const column of columns.split(',')) {
+                await octokit.projects.createColumn({
+                    project_id: response.data.id,
+                    name: column.trim()
+                })
+            }
+        }
+    } catch (err) {
+        core.setFailed(err.message)
+    }
+}
+
+run()
